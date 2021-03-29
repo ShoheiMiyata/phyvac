2ee82b241f356bbfe0820705dcb81f28d8f7b9dc
Shohei Miyata <shhmyt@gmail.com>
2021-03-29 13:08:33 +0900

    Update FlowBalance_1AHU_3Chillers.py

--- Documents/Tutorials/FlowBalance_1AHU_3Chillers.py
+++ Documents/Tutorials/FlowBalance_1AHU_3Chillers.py
@@ -5,17 +5,16 @@ Created on Mon Mar 29 08:52:36 2021
 @author: shhmy
 """
 
-
 import phyvac as pv
 
 # 機器の定義
-AHU1 = pv.AHU_simple(kr=2.0)
+AHU = pv.AHU_simple(kr=2.0)
 Vlv_AHU = pv.Valve(cv_max=800,r=100)
 CP1 = pv.Pump(pg=[233.9,5.9578,-4.460], eg=[0.009964,0.4174,-0.0508])
 CP2 = pv.Pump(pg=[233.9,5.9578,-4.460], eg=[0.009964,0.4174,-0.0508])
 CP3 = pv.Pump(pg=[233.9,5.9578,-4.460], eg=[0.009964,0.4174,-0.0508])
 # 枝の定義
-Branch_aAHUb = pv.Branch01(Vlv_AHU, kr_eq=AHU1.kr, kr_pipe=3.0)
+Branch_aAHUb = pv.Branch01(Vlv_AHU, kr_eq=AHU.kr, kr_pipe=3.0)
 Branch_bChiller1a = pv.Branch10(pump=CP1, kr_eq=10.0, kr_pipe=7.0)
 Branch_bChiller2a = pv.Branch10(pump=CP2, kr_eq=10.0, kr_pipe=7.0)
 Branch_bChiller3a = pv.Branch10(pump=CP3, kr_eq=10.0, kr_pipe=7.0)
