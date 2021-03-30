cb106875e15244bce07423d6f81fb998ca3610b2
AkariNomura <78840483+AkariNomura@users.noreply.github.com>
2021-03-30 16:04:41 +0900

    Update phyvac.py
    ダンパ曲線の二次関数化、p2f関数の追加

--- phyvac.py
+++ phyvac.py
@@ -1089,32 +1089,56 @@ class W2a_hex():
     
 # ダンパ特性と圧力損失計算
 class Damper():
-    def cal(self, g, damp,
-            coef=[[1.0,-0.00001944,0.018,0.18,-0.007], [0.8,0.0000864,0.036,0.132,0.0684],
-                 [0.6,0.001296,0.072,0.384,0.1001], [0.4,0.00108,0.36,-0.582,0.0662], [0.2,-0.0216,4.32,-5.34,0.2527]]):
+    def __init__(self, coef = [[1.0, 0.0000203437802163088], [0.8, 0.0000495885440290287],
+                    [0.6, 0.000143390887269431], [0.4, 0.000508875127863876], [0.2, 0.00351368187709778]]):
         # damp  :ダンパ開度[0.0~1.0]
-        # dp    :圧力損失[Pa]
+        # dp    :圧力損失[kPa]  # 単位注意
         # g     :流量[m^3/min]
-        # dp = a * g^3 + b * g^2 + c * g + d
-        # coef = [[damp1, a_damp1, b_damp1, c_damp1, d_damp1], ... , [dampn, a_dampn, b_dampn, c_dampn, d_dampn]]  (x1 >...> xn, n>=3)
+        # dp = a * g^2
+        # coef = [[damp1, a_damp1], ... , [dampn, a_dampnf2p]]
+        # (x1 >...> xn, n>=3)
+        self.coef = coef
+        self.g = 0
+        self.damp = 0
+        self.dp = 0
+
+    def f2p(self, damp, g):
+        n = len(self.coef)
         self.g = g
         self.damp = damp
-        self.coef = coef
-        n = len(self.coef)
-        if self.damp >= self.coef[0][0]:
-            self.dp = (self.coef[0][1] * self.g ** 3 + self.coef[0][2] * self.g ** 2 + self.coef[0][3] * self.g + self.coef[0][4])
-        elif self.damp < self.coef[n-1][0]:
-            self.dp = (self.coef[n-1][1] * self.g ** 3 + self.coef[n-1][2] * self.g ** 2 + self.coef[n-1][3] * self.g + self.coef[n-1][4])
+        if damp >= self.coef[0][0]:
+            self.dp = self.coef[0][1] * self.g ** 2  # [kPa]
+        elif damp <= self.coef[n - 1][0]:
+            self.dp = self.coef[n - 1][1] * self.g ** 2  # [kPa]
         else:
-            for i in range(1, n):    # 線形補間の上限・下限となる曲線を探す
-                self.hl = self.coef[i-1]  # higher limit curve
-                self.ll = self.coef[i]    # lower limit curve
-                if self.ll[0] <= self.damp < self.hl[0]:
+            for i in range(1, n):  # 線形補間の上限・下限となる曲線を探す
+                coef_a = self.coef[i - 1]  # higher limit curve
+                coef_b = self.coef[i]  # lower limit curve
+                if coef_b[0] <= self.damp < coef_a[0]:
                     break
-            dp_h = (self.hl[1] * self.g ** 3 + self.hl[2] * self.g ** 2 + self.hl[3] * self.g + self.hl[4])
-            dp_l = (self.ll[1] * self.g ** 3 + self.ll[2] * self.g ** 2 + self.ll[3] * self.g + self.ll[4])
-            self.dp = (dp_h - dp_l) / (self.hl[0]-self.ll[0]) * (self.damp - self.ll[0]) + dp_l
+            a = coef_a[1] * self.g ** 2
+            b = coef_b[1] * self.g ** 2
+            self.dp = (a - b) / (coef_a[0] - coef_b[0]) * (self.damp - coef_b[0]) + b
         return self.dp
+
+    def p2f(self, damp, dp):
+        n = len(self.coef)
+        self.dp = dp
+        self.damp = damp
+        if damp >= self.coef[0][0]:
+            self.g = pow(self.dp/self.coef[0][1], 0.5)
+        elif damp <= self.coef[n - 1][0]:
+            self.g = pow(self.dp/self.coef[n - 1][1], 0.5)
+        else:
+            for i in range(1, n):  # 線形補間の上限・下限となる曲線を探す
+                coef_a = self.coef[i - 1]  # higher limit curve
+                coef_b = self.coef[i]  # lower limit curve
+                if coef_b[0] <= self.damp < coef_a[0]:
+                    break
+            a = pow(self.dp/coef_a[1], 0.5)
+            b = pow(self.dp/coef_b[1], 0.5)
+            self.g = (a - b) / (coef_a[0] - coef_b[0]) * (self.damp - coef_a[0]) + a
+        return self.g
 
     
 # ファン特性と消費電力計算
