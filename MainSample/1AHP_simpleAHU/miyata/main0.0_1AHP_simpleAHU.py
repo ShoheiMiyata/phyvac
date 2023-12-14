a94b9d928f88aeccd91ba63b18b31d1565ee3a7e
Shohei Miyata <shhmyt@gmail.com>
2023-12-14 09:33:37 +0900

    Update main0.0_1AHP_simpleAHU.py

--- MainSample/1AHP_simpleAHU/miyata/main0.0_1AHP_simpleAHU.py
+++ MainSample/1AHP_simpleAHU/miyata/main0.0_1AHP_simpleAHU.py
@@ -1,6 +1,3 @@
-"""
-@author: akrnmr
-"""
 # AHP1　   空冷ヒートポンプ
 # CP1   　冷温水１次ポンプ
 # AHU　   空調機
@@ -28,13 +25,11 @@ output_data = output_data.assign(g_load=0.0, AHU_g=0.0, AHU_tin=0.0, AHU_tout=0.
 AHU = pv.AHU_simple(kr=1000)  # HEXのAHUであり、単位は[kPa/(m3/min)^2]だがkr=1000で、オーダー大丈夫？なおパイプのKr=500
 Vlv_AHU = pv.Valve(cv_max=40, r=100)
 CP1 = pv.Pump(pg=[108.22, 37.32, -1543.39], eg=[0, 5.6657, -13.8139], r_ef=0.8)
-AHP1 = pv.AirSourceHeatPump(spec_table=pd.read_excel('equipment_spec.xlsx', sheet_name='AirSourceHeatPump',encoding="SHIFT-JIS",header=None))
+AHP1 = pv.AirSourceHeatPump(filename='equipment_spec.xlsx', sheet_name='AirSourceHeatPump')
 
 # 枝の定義
-# Branch01  機器、バルブを有する枝
-# Branch10  ポンプ・機器を有する枝
-Branch_aAHUb = pv.Branch01(valve=Vlv_AHU, kr_eq=AHU.kr, kr_pipe=1000)
-Branch_bAHP1a = pv.Branch10(pump=CP1, kr_eq=AHP1.kr_ch, kr_pipe=1000)
+Branch_aAHUb = pv.BranchW(valve=Vlv_AHU, kr_eq=AHU.kr, kr_pipe=1000)
+Branch_bAHP1a = pv.BranchW(pump=CP1, kr_eq=AHP1.kr_ch, kr_pipe=1000)
 
 # 制御の定義
 PID_AHU_Vlv = pv.PID(kp=0.3, ti=400)
@@ -45,18 +40,18 @@ current_time = datetime.datetime(2018, 9, 21, 0, 0)
 for calstep in tqdm(range(24*60*4)):
     g_load = input_data.iat[calstep, 0]        # 負荷流量[m3/min]
     q_load = input_data.iat[calstep, 1]        # 負荷熱量[MJ/min]
-    t_supply_sv = input_data.iat[calstep, 2]   # 供給水温設定値[℃]
+    t_supply_sp = input_data.iat[calstep, 2]   # 供給水温設定値[℃]
     tdb = input_data.iat[calstep, 3]           # 外気乾球温度[℃]
     rh = input_data.iat[calstep, 4]            # 外気相対湿度[%](0~100)
 
     # 設定値演算
-    AHP1.tout_ch_d = t_supply_sv
+    AHP1.tout_ch_d = t_supply_sp
 
     # 制御
     # 昼間
     if current_time.hour >= 9 and current_time.hour < 18:  # 9:00~18:00に運転
-        Vlv_AHU.vlv = PID_AHU_Vlv.control(sv=g_load, mv=Vlv_AHU.g)
-        CP1.inv = PID_CP1.control(sv=50, mv=-Branch_aAHUb.dp) # ポンプはヘッダ間差圧を参照して制御
+        Vlv_AHU.vlv = PID_AHU_Vlv.control(sp=g_load, mv=Vlv_AHU.g)
+        CP1.inv = PID_CP1.control(sp=50, mv=-Branch_aAHUb.dp) # ポンプはヘッダ間差圧を参照して制御
         
         # 流量計算
         # 冷水系
@@ -83,6 +78,7 @@ for calstep in tqdm(range(24*60*4)):
         CP1.inv = 0
         
         Branch_aAHUb.f2p(0)
+        Branch_aAHUb.dp=0
         AHU.g = 0
         AHP1.g = 0
         CP1.g = 0
@@ -94,7 +90,7 @@ for calstep in tqdm(range(24*60*4)):
 
     # 現時刻の温度・消費電力計算
     AHU.cal(g=Vlv_AHU.g, tin=AHP1_0.tout_ch, q_load=q_load)
-    AHP1.cal(tout_ch_sv=t_supply_sv, tin_ch=AHU_0.tout, g_ch=CP1.g, tdb=tdb)
+    AHP1.cal(tout_ch_sp=t_supply_sp, tin_ch=AHU_0.tout, g_ch=CP1.g, tdb=tdb)
     CP1.cal()
 
     output_data.iloc[calstep] = np.array([[g_load, AHU.g, AHU.tin, AHU.tout, Vlv_AHU.vlv, -Branch_aAHUb.dp, CP1.g, CP1.inv, CP1.pw, CP1.dp, CP1.ef,
